445d05c5dd1561d944c3e74ed38294d0c415a9a8
Jon Roberts <kewlguy@gmail.com>
2020-08-31 14:15:01 -0600

    Added Model and Controller for Todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+#Setting it as "home" page for the server
+  root "todos#index"
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #using "todos" mode
+resources :todos
 end
