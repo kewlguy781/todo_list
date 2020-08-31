39e91660683546731a487da6586ae2ca3457fa8c
Jon Roberts <kewlguy@gmail.com>
2020-08-31 15:16:20 -0600

    add all controllers and models, including edit page for task

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,53 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update]
+
   def index
+    @todos = current_user.todos
   end
 
   def show
   end
 
   def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = current_user.todos.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todos_path
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:name, :status, :task)
+    end
+
+    def set_todo
+
+      #note: it was not in the instructor's code, but as per mentioned in LMS, going to 
+      #add it here to see what the result would be. 
+
+      @todo = current_user.todos.find(params[:id])
+    end
+
 end
